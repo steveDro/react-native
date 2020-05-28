4789a1d89ea50ee8e7262486c7fc52563fa869bc
steveDro <akotosteven@gmail.com>
2020-05-28 11:22:42 +0000

    CH01-02-Fund-concept

--- App.js
+++ App.js
@@ -1,19 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  Image,
+  SafeAreaView,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello react native</Text>
+      <Button title="Click Me!" onPress={() => console.log("Clicked")} />
+      <Button title="Click Me 2!" onPress={() => alert("Clicked")} />
+      <Button
+        title="Click Me 3!"
+        // onPress={() =>
+        //   Alert.alert("Alert", "You clicked Me", [
+        //     {
+        //       text: "Yes",
+        //       onPress: () => Alert.alert("Yes clicked", "Why did u click me!"),
+        //     },
+        //     { text: "No" },
+        //   ])
+        // }
+        onPress={() =>
+          Alert.prompt("Prompt", "Message", (text) => console.log(text))
+        }
+      />
+      {/* <Image source={require("./assets/icon.png")} /> */}
+      {/* <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 200,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableWithoutFeedback> */}
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 200,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+
+      {/* <image source={{ uri: "https://picsum.photos/200/300" }} /> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
