40e52d3997b26b486ed9c478122a01915235e558
steveDro <akotosteven@gmail.com>
2020-05-28 21:25:49 +0000

    Building welcome screen

--- App.js
+++ App.js
@@ -1,60 +1,10 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableWithoutFeedback,
-  TouchableHighlight,
-  Image,
-  SafeAreaView,
-  Button,
-  Alert,
-} from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello react native</Text>
-      <Button title="Click Me!" onPress={() => console.log("Clicked")} />
-      <Button title="Click Me 2!" onPress={() => alert("Clicked")} />
-      <Button
-        title="Click Me 3!"
-        // onPress={() =>
-        //   Alert.alert("Alert", "You clicked Me", [
-        //     {
-        //       text: "Yes",
-        //       onPress: () => Alert.alert("Yes clicked", "Why did u click me!"),
-        //     },
-        //     { text: "No" },
-        //   ])
-        // }
-        onPress={() =>
-          Alert.prompt("Prompt", "Message", (text) => console.log(text))
-        }
-      />
-      {/* <Image source={require("./assets/icon.png")} /> */}
-      {/* <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
-        <Image
-          source={{
-            width: 200,
-            height: 200,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableWithoutFeedback> */}
-      <TouchableHighlight onPress={() => console.log("Image tapped")}>
-        <Image
-          source={{
-            width: 200,
-            height: 200,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
-
-      {/* <image source={{ uri: "https://picsum.photos/200/300" }} /> */}
-    </SafeAreaView>
-  );
+  return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({
