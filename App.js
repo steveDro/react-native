1323367ad70df9d7ec744f322474ed90bf872bfd
steveDro <akotosteven@gmail.com>
2020-05-28 16:56:37 +0000

    add layouts

--- App.js
+++ App.js
@@ -1,67 +1,59 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableWithoutFeedback,
-  TouchableHighlight,
-  Image,
-  SafeAreaView,
-  Button,
-  Alert,
-} from "react-native";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello react native</Text>
-      <Button title="Click Me!" onPress={() => console.log("Clicked")} />
-      <Button title="Click Me 2!" onPress={() => alert("Clicked")} />
-      <Button
-        title="Click Me 3!"
-        // onPress={() =>
-        //   Alert.alert("Alert", "You clicked Me", [
-        //     {
-        //       text: "Yes",
-        //       onPress: () => Alert.alert("Yes clicked", "Why did u click me!"),
-        //     },
-        //     { text: "No" },
-        //   ])
-        // }
-        onPress={() =>
-          Alert.prompt("Prompt", "Message", (text) => console.log(text))
-        }
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row", // row-reverse, column-reverse, column
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+        alignContent: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 300,
+          alignSelf: "flex-start",
+        }}
       />
-      {/* <Image source={require("./assets/icon.png")} /> */}
-      {/* <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
-        <Image
-          source={{
-            width: 200,
-            height: 200,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableWithoutFeedback> */}
-      <TouchableHighlight onPress={() => console.log("Image tapped")}>
-        <Image
-          source={{
-            width: 200,
-            height: 200,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
-
-      {/* <image source={{ uri: "https://picsum.photos/200/300" }} /> */}
-    </SafeAreaView>
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 200,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+      {/* <View
+        style={{
+          backgroundColor: "dodgerblue",
+          flex: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          flex: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          flex: 1,
+        }}
+      /> */}
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
