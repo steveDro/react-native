c26aa46d3876fb2f0ad84b7c3e2add6af96bf94e
steveDro <akotosteven@gmail.com>
2020-05-30 12:02:58 +0000

    Improved the welcome screen

--- App.js
+++ App.js
@@ -1,49 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, Button } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { View } from "react-native";
+import AppButton from "./app/component/AppButton";
 
-import AppText from "./app/component/AppText";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <MaterialCommunityIcons name="email" size={40} color="dodgerblue" />
-      <AppText>This is encapsulated text</AppText>
-      <Text
-        style={{
-          fontFamily: "Courier",
-          fontSize: 30,
-          fontWeight: "600",
-          color: "tomato",
-          fontStyle: "italic",
-          textTransform: "capitalize",
-          textAlign: "justify",
-          lineHeight: 25,
-        }}
-      >
-        I love react native. This is first my time programming in react
-      </Text>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          height: 100,
-          width: 100,
-          shadowColor: "grey",
-          shadowOpacity: 1,
-          shadowRadius: 20,
-          shadowOffset: { width: 10, height: 10 },
-          // borderWidth: 10,
-          // borderColor: "royalblue",
-          // // borderTopWidth: 20,
-          // borderRadius: 50,
-        }}
-      ></View>
-    </View>
-  );
+  return <WelcomeScreen />;
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AppButton from "../component/AppButton";
 
 function WelcomeScreen() {
   return (
     <ImageBackground
+      blurRadius={5}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>Sell What You Don't Need</Text>
+        <Text style={styles.tagLine}>Sell What You Don't Need</Text>
       </View>
 
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonContainer}>
+        <AppButton title="Login" />
+        <AppButton title="Register" color="secondary" />
+      </View>
     </ImageBackground>
   );
 }
@@ -24,10 +28,9 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
   },
-  loginButton: {
+  buttonContainer: {
     width: "100%",
-    height: 70,
-    backgroundColor: "#fc5c65",
+    padding: 20,
   },
   logo: {
     width: 100,
@@ -42,6 +45,11 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 70,
     backgroundColor: "#4ecdc4",
+  },
+  tagLine: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 10,
   },
 });
 
