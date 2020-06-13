80ab53975d762bdbb916bde8f2a230240c12d9be
steveDro <akotosteven@gmail.com>
2020-06-13 07:46:16 +0000

    added navigation

--- App.js
+++ App.js
@@ -1,45 +1,112 @@
 import React, { useState, useEffect } from "react";
-import { Button, Image } from "react-native";
+import { Text, Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Screen from "./app/component/Screen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import Icon from "./app/component/Icon";
-import ListItem from "./app/component/list/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import AppInputText from "./app/component/AppInputText";
-import { Switch } from "react-native";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import AppFormPicker from "./app/component/forms/AppFormPicker";
-import { useFormikContext } from "formik";
-import { AppFormField } from "./app/component/forms";
-import ListEditScreen from "./app/screens/ListEditScreen";
-import AppImagePicker from "./app/component/device_features/ImagePicker";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
 
-import * as ImagePicker from "expo-image-picker";
-
-export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  handleRemove = (uri) => {
-    console.log(uri);
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
-
+// using props - {navigation}
+const Tweets = ({ navigation }) => {
   return (
-    <ListEditScreen />
-    // <Screen>
-    //   <ImageInputList
-    //     imageUris={imageUris}
-    //     onAddImage={handleAdd} // {(uri) => handleAdd(uri)}
-    //     onRemoveImage={handleRemove}
-    //   />
-    // </Screen>
+    <Screen>
+      <Text>Tweets</Text>
+      <Button
+        title="View tweet details"
+        onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+      />
+      {/* child component */}
+      <Links />
+    </Screen>
+  );
+};
+// using hooks - useNavigation() for child components
+const Links = () => {
+  const navigation = useNavigation();
+  return (
+    <Button title="click" onPress={() => navigation.navigate("TweetDetails")} />
+  );
+};
+const TweetDetails = ({ route }) => {
+  return (
+    <Screen>
+      <Text>TweetDetails {route.params.id}</Text>
+    </Screen>
+  );
+};
+
+const Account = () => {
+  return (
+    <Screen>
+      <Text>Account</Text>
+    </Screen>
+  );
+};
+
+const Stack = createStackNavigator();
+const StackNavigation = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "dodgerblue" },
+        headerTintColor: "white",
+      }}
+    >
+      <Stack.Screen
+        name="Tweets"
+        component={Tweets}
+        options={{
+          headerStyle: { backgroundColor: "tomato" },
+          headerTintColor: "white",
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="TweetDetails"
+        component={TweetDetails}
+        // options={{ title: "Tweet Details" }}
+        options={({ route }) => ({ title: route.params.id })} // setting titles dynamically
+      />
+    </Stack.Navigator>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeBackgroundColor: "tomato",
+        activeTintColor: "white",
+        inactiveBackgroundColor: "#eee",
+        inactiveTintColor: "black",
+      }}
+    >
+      <Tab.Screen
+        name="Tweets"
+        component={StackNavigation}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen name="Account" component={Account} />
+    </Tab.Navigator>
+  );
+};
+export default function App() {
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      {/* <StackNavigation /> */}
+      {/* <TabNavigation /> */}
+      {/* <AuthNavigator /> */}
+      <AppNavigator />
+    </NavigationContainer>
   );
 
   // <ListEditScreen />;
---
+++ app/navigation/AuthNavigator.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import WelcomeScreen from "../screens/WelcomeScreen";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
+
+const AuthNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthNavigator;
